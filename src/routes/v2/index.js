269ca540729a043f50042265d19072a781873b8f
mikkamolthep <mik.kamolthep@gmail.com>
2026-01-12 11:08:51 +0700

    edited bugs

--- src/routes/v2/index.js
+++ src/routes/v2/index.js
@@ -1,9 +1,6 @@
 import { Router } from "express";
 import { router as usersRoutes } from "./users.routes.js";
-import healthRoutes from "./health.routes.js"
 
 export const router = Router();
 
 router.use("/users", usersRoutes);
-
-router.use("/health", healthRoutes)
